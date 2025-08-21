dda9119a170f6a449acf1c60361aa4eb72c30d48
Ksawlii <maja.blaszczak@proton.me>
2025-08-21 15:18:44 +0200

    a33x: Initial proprietary-firmware.txt list
    Change-Id: I4833aeb1f7383a01f197a9e672cf89f5130811c7
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- Android.mk
+++ Android.mk
@@ -6,5 +6,18 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),a33x)
 
+$(call add-radio-file-sha1-checked,radio/fld.bin_SM-A336B,0d748abe10da1b0fe57f205b0ba8bdccb79bd3c4)
+$(call add-radio-file-sha1-checked,radio/harx.bin_SM-A336B,2b56a905d0c3ac1f95cf6c014dcdddd5f48bd0a5)
+$(call add-radio-file-sha1-checked,radio/keystorage.bin_SM-A336B,90f414bd8f1ae4d1b7f9f79fa26a05d28c960217)
+$(call add-radio-file-sha1-checked,radio/ldfw.img_SM-A336B,44fc00f01739623a570c52328509bd2fdf9fbbbb)
+$(call add-radio-file-sha1-checked,radio/models,c3f36bf5e129b5b8f37e732287479b456ce2fb26)
+$(call add-radio-file-sha1-checked,radio/modem.bin_SM-A336B,60181fa05b7e86b5b1495ca6fe4b0f48d8eec5e1)
+$(call add-radio-file-sha1-checked,radio/modem_debug.bin_SM-A336B,203a495a068be98b5e53c4b5e2d60b5e050176b6)
+$(call add-radio-file-sha1-checked,radio/sboot.bin_SM-A336B,a4d067af33d2045c8f0ecc64555129fb773acebd)
+$(call add-radio-file-sha1-checked,radio/tzar.img_SM-A336B,181e0484fae0355248d4d3a9684b996988689f07)
+$(call add-radio-file-sha1-checked,radio/tzsw.img_SM-A336B,4aa4547d263e6aded28c7d86cbb554a8d8c822f1)
+$(call add-radio-file-sha1-checked,radio/uh.bin_SM-A336B,98a1ebf2c0d0a8bb6fac5177152162d71783e709)
+$(call add-radio-file-sha1-checked,radio/up_param.bin_SM-A336B,2432974a84788d9445d0b2f8dcccd2cd2c64d4a8)
+$(call add-radio-file-sha1-checked,radio/version_SM-A336B,b3e657e0d2762169ec4d84494be3cfd327e61226)
 
 endif
